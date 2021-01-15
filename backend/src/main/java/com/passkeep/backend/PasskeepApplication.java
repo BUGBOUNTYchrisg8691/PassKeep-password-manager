542489fc59837e2a7b731a65e84c5e6b96661152
Christopher Girvin <bugbountychrisg8691@gmail.com>
2021-01-14 22:24:56 -0500

    init commit

---
+++ backend/src/main/java/com/passkeep/backend/PasskeepApplication.java
@@ -0,0 +1,13 @@
+package com.passkeep.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasskeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasskeepApplication.class, args);
+	}
+
+}
