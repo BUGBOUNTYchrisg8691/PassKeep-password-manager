9140dd09eb4cb4847c2257a07f0dac0fc8dff4fa
Christopher Girvin <bugbountychrisg8691@gmail.com>
2021-01-15 00:28:50 -0500

    all base project deps installed

--- server/src/test/java/com/passkeep/server/PassKeepApplicationTests.java
+++ server/src/test/java/com/passkeep/server/PassKeepApplicationTests.java
@@ -1,13 +1,15 @@
-package com.passkeep.backend;
+package com.passkeep.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PasskeepApplicationTests {
+class PassKeepApplicationTests
+{
 
 	@Test
-	void contextLoads() {
+	void contextLoads()
+	{
 	}
 
 }
