e2f6ba466761f2f0b6d5d1a9c7fc15ba97a8b118
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2021-01-22 16:36:11 -0500

    new password form created

--- client/src/components/NewPassForm.js
+++ client/src/components/NewPassForm.js
@@ -1,51 +1,53 @@
-import { useState } from 'react'
+import { useState } from 'react';
+
+import axios from 'axios';
 
 let initialFormVals = {
-  len: '',
-  specChars: '',
-}
+  len: "",
+  specChars: "",
+};
 
-function NewPassForm() {
+function NewPassForm({ newPassword }) {
   const [getFormVals, setFormVals] = useState(initialFormVals);
 
   const handleChange = (e) => {
-    const [name, value] = e.target
+    const [name, value] = e.target;
     setFormVals({
       ...getFormVals,
-      [name]: value
-    })
-    return
-  }
+      [name]: value,
+    });
+    return;
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    axios.post('http://localhost:5000/passgen', getFormVals)
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/passgen", getFormVals)
       .then((res) => {
-        console.log(res)
+        console.log(res);
       })
       .catch((err) => {
-        console.log(err)
-      })
-    setFormVals(initialFormVals)
-  }
+        console.log(err);
+      });
+    setFormVals(initialFormVals);
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor='len'>Length</label>
-      <input
-        name='len'
-        value={getFormVals.len}
-        onChange={handleChange}
-      />
-      <label htmlFor='specChars'>Special Characters</label>
-      <input
-        name='specChars'
-        value={getFormVals.specChars}
-        onChange={handleChange}
-      />
-      <button>Generate Password</button>
-    </form>
-  )
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="len">Length</label>
+        <input name="len" value={getFormVals.len} onChange={handleChange} />
+        <label htmlFor="specChars">Special Characters</label>
+        <input
+          name="specChars"
+          value={getFormVals.specChars}
+          onChange={handleChange}
+        />
+        <button>Generate Password</button>
+      </form>
+      {newPassword && <p>{newPassword}</p>}
+    </>
+  );
 }
 
 export default NewPassForm;
